73e41e93ef83e206aa17d6fc6cbdbbc71f8382aa
Ryhor Volasau <G.volosov@gmail.com>
2023-04-17 13:40:14 +0400

    feat: add solution extended-repeater 04/17/2023 13:40:14

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,69 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let str2, repeatTimes, additionRepeatTimes, separator, additionSeparator;
+  str = String(str);
+  if (options.addition) {
+    str2 = String(options.addition);
+  } else {
+    str2 = "";
+  }
+
+  if (options.repeatTimes) {
+    repeatTimes = options.repeatTimes;
+  } else {
+    repeatTimes = 1;
+  }
+  if (options.additionRepeatTimes) {
+    additionRepeatTimes = options.additionRepeatTimes;
+  } else {
+    additionRepeatTimes = 1;
+  }
+  if (options.separator) {
+    separator = options.separator;
+  } else {
+    separator = "+";
+  }
+  if (options.additionSeparator) {
+    additionSeparator = options.additionSeparator;
+  } else {
+    additionSeparator = "|";
+  }
+
+  let result = "";
+
+  for (let i = 0; i < repeatTimes; i++) {
+    result += str;
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      result += str2;
+      if (j < additionRepeatTimes - 1) {
+        result += additionSeparator;
+      }
+    }
+
+    if (i < repeatTimes - 1) {
+      result += separator;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
